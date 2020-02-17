b69fbd13d4b7e9079468179fda5f5ccad8b67c33
Steven <38477361+Steven-matos@users.noreply.github.com>
2020-02-17 17:28:25 -0500

    set form and list on app.js

--- reducer-todo/src/App.js
+++ reducer-todo/src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useReducer} from 'react';
+import './Todo.css';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+import {todoReducer, initialState} from './reducer/todoReducer'
 
 function App() {
+  const [state, dispatch] = useReducer(todoReducer, initialState);
+  const [newTask, setNewTask] = useState('');
+
+  // Form Change handler
+  const handleChanges = e => {
+    setNewTask(e.target.value);
+  };
+
+  // Form Submit handler
+  const handleSubmit = e => {
+    e.preventDefault();
+    dispatch({ type: 'ADD_TODO', payload: newTask })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1>To-do App!</h1>
+      <h2>List</h2>
+      <TodoForm handleChanges={handleChanges} handleSubmit={handleSubmit}/>
+      <TodoList taskList={state} dispatch={dispatch}/>
     </div>
   );
 }
